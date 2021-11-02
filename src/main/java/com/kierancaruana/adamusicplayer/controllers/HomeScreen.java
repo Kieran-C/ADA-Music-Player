75647b4f81775d07f98272e6b45906c9b3cb0c3d
Kieran-C <kieran.caruana@gmail.com>
2021-11-02 12:31:24 +0000

    Added ability to populate home screen song table

--- src/main/java/com/kierancaruana/adamusicplayer/controllers/HomeScreen.java
+++ src/main/java/com/kierancaruana/adamusicplayer/controllers/HomeScreen.java
@@ -2,12 +2,13 @@ package com.kierancaruana.adamusicplayer.controllers;
 
 import com.kierancaruana.adamusicplayer.helpers.music.MusicControls;
 import com.kierancaruana.adamusicplayer.objects.Song;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
@@ -15,22 +16,31 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 
+import java.awt.event.MouseEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class HomeScreen extends StackPane {
 
     @FXML
     private VBox musicVbox;
     @FXML
-    private TableView trackTable;
+    private TableView<Song> trackTable;
     @FXML
     private TableColumn trackNameColumn;
     @FXML
     private VBox playlistVbox;
     @FXML
     private Label playlistsTitleBar;
+    @FXML
+    private ProgressBar songProgress;
 
     MusicControls musicControls = new MusicControls();
 
+    ObservableList<Song> songList = FXCollections.observableArrayList();
+
+    Logger logger = Logger.getLogger(StageManager.class.getName());
 
     @FXML
     public void initialize() {
@@ -43,9 +53,42 @@ public class HomeScreen extends StackPane {
         playlistsTitleBar.setTextFill(Color.rgb(255,255,255));
         trackTable.setStyle("-fx-background-color: #28353b; -fx-alternative-row-fill-visible: #3a4c53;");
         trackTable.setPrefWidth(((winWidth-20)/100)*90);
+        songProgress.setStyle("-fx-background-insets: 0; -fx-padding: 0.75em; -fx-background-radius: 2;");
         BackgroundFill bf = new BackgroundFill(Color.rgb(46, 46, 46), CornerRadii.EMPTY, Insets.EMPTY);
         Background bg = new Background(bf);
         musicVbox.setBackground(bg);
+
+        logger.log(Level.INFO, "Home screen formatting completed");
+
+        trackTable.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
+
+        });
+
+        logger.log(Level.INFO, "Listener Created");
+
+        Song song = new Song();
+        song.setTrackId(1);
+        song.setTrackName("TestSong");
+        song.setTrackFileLocation("/music/Noisestorm-CrabRave.mp3");
+        songList.add(song);
+        trackTable.setItems(songList);
+        logger.log(Level.INFO, "Test song object created");
+
+
+        trackTable.setRowFactory(param -> {
+            final TableRow<Song> tableRow = new TableRow<>();
+            tableRow.setOnMouseClicked(mouseEvent -> {
+                if (mouseEvent.getButton() == MouseButton.PRIMARY){
+                    logger.log(Level.INFO, "Primary mouse button clicked");
+                    String fileLocation = trackTable.getSelectionModel().getSelectedItem().getTrackFileLocation();
+                    musicControls.playMp3(fileLocation);
+                }
+            });
+            return tableRow;
+        });
+
+        logger.log(Level.INFO, "Mouse event created");
+
     }
 
     public void onPlayButtonClick() {
