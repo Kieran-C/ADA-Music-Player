58e8197945bee129d549ac50d1aeadf396a1a9be
Kieran-C <kieran.caruana@gmail.com>
2021-11-03 00:12:59 +0000

    Added backend code to manage the idle timeout feature

---
+++ src/main/java/com/kierancaruana/adamusicplayer/helpers/Idle.java
@@ -0,0 +1,36 @@
+package com.kierancaruana.adamusicplayer.helpers;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.lang.reflect.Array;
+
+public class Idle extends Thread{
+    public void run() {
+        boolean active = true;
+        int[] lastSavedMouseCoords = {900000,900000};
+        while (true){
+            int delay;
+            if (active){delay = 30000;}
+            else{delay = 100;}
+            int currentX = MouseInfo.getPointerInfo().getLocation().x;
+            int currentY = MouseInfo.getPointerInfo().getLocation().y;
+            try {
+                if ((lastSavedMouseCoords[0] != currentX) || (lastSavedMouseCoords[1] != currentY)){
+                    active = true;
+                }else{
+                    active = false;
+                }
+                lastSavedMouseCoords[0] = currentX;
+                lastSavedMouseCoords[1] = currentY;
+                sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Active Status: " +active);
+        }
+    }
+//    public int[] handle(MouseEvent me){
+//        int[] meCoords = {me.getX(),me.getY()};
+//        return meCoords;
+//    }
+}
